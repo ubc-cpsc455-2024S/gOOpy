ffcc9415ee1d883e388eae4d66fddd59b3a9a113
AidenKerr <arkerr2000@gmail.com>
2024-05-26 15:06:09 -0700

    Add basic raymarching

--- gOOpy-frontend/src/pages/Editor/Editor.jsx
+++ gOOpy-frontend/src/pages/Editor/Editor.jsx
@@ -1,5 +1,111 @@
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
+import { FileLoader, Vector3 } from 'three';
+
+const fragmentShaderPath = '/shaders/raymarching.fs.glsl';
+const vertexShaderPath = '/shaders/raymarching.vs.glsl';
+
+function RayMarching({ testPos, uniforms, ...props }) {
+    const { clock } = useThree();
+    const [vertexShader, fragmentShader] = useLoader(FileLoader, [
+        vertexShaderPath,
+        fragmentShaderPath,
+    ]);
+
+    // this is just for fun. remove when actual editor built
+    useFrame(() => {
+        uniforms.current.spheres.value[2].center.y = Math.sin(
+            clock.getElapsedTime()
+        );
+        uniforms.current.spheres.value[2].center.z =
+            -0.5 + 0.5 * Math.sin(clock.getElapsedTime());
+    });
+
+    return (
+        <mesh {...props}>
+            <planeGeometry />
+            <rawShaderMaterial
+                fragmentShader={fragmentShader}
+                vertexShader={vertexShader}
+                uniforms={uniforms.current}
+            />
+        </mesh>
+    );
+}
+
 function Editor() {
-    return <h1>Editor</h1>;
+    // hard coded list of objects
+    const obj1 = {
+        center: new Vector3(0.0, 0.0, 0.0),
+        radius: 1.0,
+    };
+    const obj2 = {
+        center: new Vector3(1.0, 1.0, 1.0),
+        radius: 1.3,
+    };
+    const obj3 = {
+        center: new Vector3(-1.0, -1.0, 1.0),
+        radius: 0.8,
+    };
+
+    // the current objects in the scene
+    const [sceneObjects, setSceneObjects] = useState([obj1, obj2, obj3]);
+
+    // unsure if this is the best strat. This lets us keep track of uniforms object to be passed to shader
+    const uniforms = useRef({
+        n_spheres: { type: 'int', value: 3 },
+        spheres: {
+            type: [{ center: 'vec3', radius: 'float' }],
+            value: sceneObjects,
+        },
+        camera_pos: {
+            type: 'vec3',
+            value: new Vector3(0.0, 0.0, -3.0),
+        },
+    });
+
+    // updates a scene object's x coordinate based on index
+    // I can see this translating to redux quite well, if we decide to go that route.
+    function updateSceneObjects(index, newValue) {
+        setSceneObjects((prev) => {
+            prev[index].center.x = newValue;
+            return prev;
+        });
+    }
+
+    return (
+        <>
+            <Canvas style={{ width: 500, height: 500 }}>
+                <RayMarching scale={[4.0, 4.0, 1.0]} uniforms={uniforms} />
+            </Canvas>
+            <div>
+                <Slider
+                    defaultValue={sceneObjects[0].center.x}
+                    index={0}
+                    updateSceneObjects={updateSceneObjects}
+                />
+            </div>
+        </>
+    );
+}
+
+function Slider({ defaultValue, index, updateSceneObjects }) {
+    const [val, setVal] = useState(defaultValue);
+    return (
+        <input
+            style={{ width: '500px' }}
+            value={val}
+            onChange={(e) => {
+                const newValue = parseFloat(e.target.value);
+                updateSceneObjects(index, newValue);
+                setVal(newValue);
+            }}
+            type='range'
+            min='-5'
+            max='5'
+            step='0.001'
+        ></input>
+    );
 }
 
 export default Editor;
