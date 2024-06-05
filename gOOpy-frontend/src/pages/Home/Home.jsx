458e5d91328a54fadb69303a2d8e30e19a07e7ed
AidenKerr <arkerr2000@gmail.com>
2024-06-05 15:00:41 -0700

    Add subheader to main page

--- gOOpy-frontend/src/pages/Home/Home.jsx
+++ gOOpy-frontend/src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
         <main className='p-10 text-center'>
             <span>Welcome to</span>
             <h1 className='text-5xl font-bold'>gOOpy</h1>
+            <h2 className='text-1xl'>The 3D scene editor</h2>
             <div className='p-10'>
                 <a href='/editor' className='hover:underline'>
                     Click here to see the editor
