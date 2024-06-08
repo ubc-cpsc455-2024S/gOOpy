7212c525d02316a10a80d960955a5fc37da08930
AidenKerr <arkerr2000@gmail.com>
2024-06-07 17:02:47 -0700

    Improved custom hook
    Co-authored-by: jlacsamana <jlacsamana@users.noreply.github.com>

--- gOOpy-frontend/src/pages/Editor/Editor.jsx
+++ gOOpy-frontend/src/pages/Editor/Editor.jsx
@@ -1,36 +1,75 @@
-import React, { useRef, useState } from 'react';
+import React, { useReducer, useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Vector3 } from 'three';
 import RayMarching from './RayMarching/RayMarching';
 
 /**
  *  useEditorData is a custom react hook to handle state management for our 3D scene editor
- *  TODO this hook should handle scene object data as described in discord (or maybe slightly differently?)
  */
 function useEditorData() {
     // hard coded list of objects (temporary)
     const obj1 = {
         center: new Vector3(0.0, 0.0, 0.0),
         radius: 1.0,
+        id: 1,
     };
     const obj2 = {
         center: new Vector3(1.0, 1.0, 1.0),
         radius: 1.3,
+        id: 2,
     };
     const obj3 = {
         center: new Vector3(-1.0, -1.0, 1.0),
         radius: 0.8,
+        id: 3,
     };
 
-    // the current objects in the scene
-    const [sceneObjects, setSceneObjects] = useState([obj1, obj2, obj3]);
+    const MAX_SIZE = 3; // should match shaders
+    let shapesCount = 3; // EDIT FIX THIS LATER (reducer initialization function for loading data?)
+    let nextID = 4; // fix later
+    const shapesMap = new Map();
+    const buffer = [...Array(MAX_SIZE)].map(() => {
+        return {
+            center: new Vector3(0.0, 0.0, 0.0),
+            radius: 1.0,
+            id: null,
+        };
+    });
+
+    // TEMPORARY SETTING BUFFER VALUES:
+    buffer[0] = obj1;
+    buffer[1] = obj2;
+    buffer[2] = obj3;
+
+    const [state, dispatch] = useReducer((state, action) => {
+        const newState = [...state]; // copy of state to modify
+        switch (action.type) {
+            case 'addShape':
+                const { shapeType } = action.payload;
+                // TODO change this based on type? or different reducer per type
+                shapesCount += 1;
+                let shapeData = buffer[shapesCount];
+                shapeData.center = new Vector3(0.0, 0.0, 0.0);
+                shapeData.radius = 1.0;
+                shapeData.radius = nextID;
+                newState.append(shapeData);
+                shapesMap.set(nextID, shapesCount - 1);
+                nextID++;
+                return newState;
+            case 'modifyAxis':
+                // temp demo action
+                const { index, newValue, axis } = action.payload;
+                newState[index].center[axis] = newValue;
+                return newState;
+        }
+    }, buffer.slice(0, 3));
 
     // unsure if this is the best strat. This lets us keep track of uniforms object to be passed to shader
     const uniforms = useRef({
-        n_spheres: { type: 'int', value: 3 },
+        n_spheres: { type: 'int', value: shapesCount },
         spheres: {
             type: [{ center: 'vec3', radius: 'float' }],
-            value: sceneObjects,
+            value: buffer,
         },
         camera_pos: {
             type: 'vec3',
@@ -38,33 +77,39 @@ function useEditorData() {
         },
     });
 
-    // UPDATE FUNCTION FOR CUSTOM HOOK
-    // TODO Eventually this will be used to sync our multiple data structures
-    // Maybe we can actually change this to use usereducer instead of usestate for this hook?
-    // https://react.dev/reference/react/useReducer
-    function setEditorData(index, newValue) {
-        setSceneObjects((prev) => {
-            prev[index].center.x = newValue;
-            return prev;
-        });
-    }
-
-    return [sceneObjects, setEditorData, uniforms];
+    return [state, dispatch, uniforms];
 }
 
 function Editor() {
-    const [editorData, setEditorData, uniforms] = useEditorData();
+    const [editorData, dispatch, uniforms] = useEditorData();
+    const [currentShape, selectShape] = useState(editorData[0].id);
 
     return (
         <div className='flex justify-between p-5'>
             <div className='sliders'>
                 <h1 className='text-3xl font-bold'>Editor</h1>
-                <Slider
+                {/* <Slider
                     defaultValue={editorData[0].center.x}
                     index={0}
-                    setEditorData={setEditorData}
-                />
+                    dispatch={dispatch}
+                /> */}
+                <div
+                    className='overflow-auto ...'
+                    style={{ minHeight: '80vh' }}
+                >
+                    {editorData.map((option, index) => (
+                        <div
+                            key={index}
+                            className='border'
+                            onClick={() => selectShape(option.id)}
+                        >
+                            Shape {option.id}
+                        </div>
+                    ))}
+                </div>
             </div>
+
+            <div className=''></div>
 
             <div>
                 <Canvas
@@ -87,14 +132,17 @@ function Editor() {
     );
 }
 
-function Slider({ defaultValue, index, setEditorData }) {
+function Slider({ defaultValue, index, dispatch, axis = 'x' }) {
     const [val, setVal] = useState(defaultValue);
     return (
         <input
             value={val}
             onChange={(e) => {
                 const newValue = parseFloat(e.target.value);
-                setEditorData(index, newValue);
+                dispatch({
+                    type: 'modifyAxis',
+                    payload: { index, newValue, axis },
+                });
                 setVal(newValue);
             }}
             type='range'
