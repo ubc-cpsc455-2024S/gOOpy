d0976e91d168c09a4441cb96237bd0e9826be94e
Jacob Lacsamana <jacoblacsamana0@gmail.com>
2024-06-21 16:50:33 -0700

    converted shape detail panel into component

--- gOOpy-frontend/src/pages/Editor/Editor.jsx
+++ gOOpy-frontend/src/pages/Editor/Editor.jsx
@@ -1,11 +1,11 @@
 import React, { useReducer, useRef, useState } from 'react';
-import { ColorPicker, useColor } from 'react-color-palette';
 import 'react-color-palette/css';
 import { Canvas } from '@react-three/fiber';
 import { Vector3 } from 'three';
 import RayMarching from './RayMarching/RayMarching';
 import GoopyButton from '../../components/GoopyButton';
 import ShapeManager from '../../components/ShapeManager';
+import ShapeDetails from '../../components/ShapeDetails';
 
 // hard coded list of objects (temporary)
 const obj1 = {
@@ -73,12 +73,13 @@ function Editor() {
                     determineNewID={determineNewID}
                 ></ShapeManager>
                 {currentShape != null && shapes.length > 0 && (
-                    <EditorDetails
+                    <ShapeDetails
                         // TODO better way to find the shapes's index?
                         index={shapes.findIndex((s) => s.id === currentShape)}
                         shapes={shapes}
                         updateAxis={updateAxis}
                         updateRadius={updateRadius}
+                        Slider={Slider}
                     />
                 )}
             </div>
@@ -99,69 +100,6 @@ function Editor() {
                 >
                     <RayMarching scale={[2.0, 2.0, 1.0]} shapes={shapes} />
                 </Canvas>
-            </div>
-        </div>
-    );
-}
-
-function EditorDetails({ index, shapes, updateAxis, updateRadius }) {
-    // TODO: change 'FF0000' to currentShape's color
-    const [color, setColor] = useColor('FF0000');
-    return (
-        <div className='sliders border ms-2' key={index}>
-            <h4 className='text-2xl font-bold'>
-                Shape {shapes[index].id} &gt; Properties
-            </h4>
-            <div className='border flex flex-col p-2'>
-                <h4 className='text-1xl font-bold'>Transform</h4>
-                <div className='flex'>
-                    <h4 className='text-1xl font-bold mr-2'>x:</h4>
-                    <Slider
-                        defaultValue={shapes[index].center.x}
-                        index={index}
-                        callback={updateAxis}
-                        callbackParams={['x']}
-                    />
-                </div>
-                <div className='flex'>
-                    <h4 className='text-1xl font-bold mr-2'>y:</h4>
-                    <Slider
-                        defaultValue={shapes[index].center.y}
-                        index={index}
-                        callback={updateAxis}
-                        callbackParams={['y']}
-                    />
-                </div>
-                <div className='flex'>
-                    <h4 className='text-1xl font-bold mr-2'>z:</h4>
-                    <Slider
-                        defaultValue={shapes[index].center.z}
-                        index={index}
-                        callback={updateAxis}
-                        callbackParams={['z']}
-                    />
-                </div>
-            </div>
-            <div className='border flex flex-col p-2'>
-                <h4 className='text-1xl font-bold'>Radius</h4>
-                <div className='flex'>
-                    <h4 className='text-1xl font-bold mr-2'>x:</h4>
-                    <Slider
-                        defaultValue={shapes[index].radius}
-                        index={index}
-                        callback={updateRadius}
-                        min={0}
-                    />
-                </div>
-            </div>
-            <div className='border flex flex-col p-2'>
-                <h4 className='text-1xl font-bold mr-2'>Colour</h4>
-                <ColorPicker
-                    color={color}
-                    onChange={setColor}
-                    hideAlpha={true}
-                    hideInput={true}
-                />
             </div>
         </div>
     );
