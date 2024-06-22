0f3d8acb187682400d51ad52412252ba077b5cf7
jlacsamana <63628806+jlacsamana@users.noreply.github.com>
2024-06-21 17:21:50 -0700

    refactor editor tabs into components (#26)
    * converted shape manager panel into a component
    
    * converted shape detail panel into component
    
    * .
    
    * created stub component for SceneManager
    
    * move key = {index} to proper div

--- gOOpy-frontend/src/pages/Editor/Editor.jsx
+++ gOOpy-frontend/src/pages/Editor/Editor.jsx
@@ -1,10 +1,11 @@
 import React, { useReducer, useRef, useState } from 'react';
-import { ColorPicker, useColor } from 'react-color-palette';
 import 'react-color-palette/css';
 import { Canvas } from '@react-three/fiber';
 import { Vector3 } from 'three';
 import RayMarching from './RayMarching/RayMarching';
 import GoopyButton from '../../components/GoopyButton';
+import ShapeManager from '../../components/ShapeManager';
+import ShapeDetails from '../../components/ShapeDetails';
 
 // hard coded list of objects (temporary)
 const obj1 = {
@@ -63,96 +64,22 @@ function Editor() {
     return (
         <div className='flex justify-between p-5'>
             <div className='flex'>
-                <div className='sliders border'>
-                    <h1 className='text-3xl font-bold'>Editor</h1>
-                    <div
-                        className='no-scrollbar overflow-y-auto border'
-                        // TODO move this custom CSS to tailwind somehow
-                        style={{
-                            height: '70vh',
-                            minWidth: '12vw',
-                        }}
-                    >
-                        {shapes.map((shape, index) => (
-                            <div className='flex justify-between'>
-                                <GoopyButton
-                                    key={index}
-                                    styleClasses={`border-b button cursor-pointer flex w-full`}
-                                    onClickBehavior={() => {
-                                        setCurrentShape(
-                                            shape.id == currentShape
-                                                ? null
-                                                : shape.id
-                                        );
-                                    }}
-                                    hovering={currentShape === shape.id}
-                                >
-                                    <p className='ps-1'>
-                                        Shape {shapes[index].id}
-                                    </p>
-                                </GoopyButton>
-                                {currentShape != shape.id && (
-                                    <GoopyButton
-                                        styleClasses={`border-l border-b pl-1 pr-1`}
-                                        onClickBehavior={(e) => {
-                                            setShapes((state) => {
-                                                const newState = [...state];
-                                                let index = newState.indexOf(
-                                                    newState.find(
-                                                        (s) => s.id == shape.id
-                                                    )
-                                                );
-                                                newState.splice(index, 1);
-                                                return newState;
-                                            });
-                                            e.stopPropagation();
-                                        }}
-                                        hovering={false}
-                                    >
-                                        <p>X</p>
-                                    </GoopyButton>
-                                )}
-                            </div>
-                        ))}
-                    </div>
-                    <GoopyButton
-                        styleClasses='border-l border-r border-b p-1'
-                        onClickBehavior={() => {
-                            const newId = determineNewID();
-                            setShapes((state) => {
-                                const newState = [...state];
-                                newState.push({
-                                    center: new Vector3(0, 0, 0),
-                                    radius: 1.0,
-                                    id: newId,
-                                });
-                                return newState;
-                            });
-                            setCurrentShape(newId);
-                        }}
-                    >
-                        Add Shape
-                    </GoopyButton>
-                    <GoopyButton
-                        styleClasses='border-l border-r border-b p-1'
-                        onClickBehavior={() => {
-                            setShapes((state) => {
-                                return [];
-                            });
-                            setCurrentShape(null);
-                        }}
-                    >
-                        Reset Scene
-                    </GoopyButton>
-                </div>
-
+                <ShapeManager
+                    shapes={shapes}
+                    currentShape={currentShape}
+                    setShapes={setShapes}
+                    setCurrentShape={setCurrentShape}
+                    setCurrIndex={setCurrIndex}
+                    determineNewID={determineNewID}
+                />
                 {currentShape != null && shapes.length > 0 && (
-                    <EditorDetails
+                    <ShapeDetails
                         // TODO better way to find the shapes's index?
                         index={shapes.findIndex((s) => s.id === currentShape)}
                         shapes={shapes}
                         updateAxis={updateAxis}
                         updateRadius={updateRadius}
+                        Slider={Slider}
                     />
                 )}
             </div>
@@ -173,69 +100,6 @@ function Editor() {
                 >
                     <RayMarching scale={[2.0, 2.0, 1.0]} shapes={shapes} />
                 </Canvas>
-            </div>
-        </div>
-    );
-}
-
-function EditorDetails({ index, shapes, updateAxis, updateRadius }) {
-    // TODO: change 'FF0000' to currentShape's color
-    const [color, setColor] = useColor('FF0000');
-    return (
-        <div className='sliders border ms-2' key={index}>
-            <h4 className='text-2xl font-bold'>
-                Shape {shapes[index].id} &gt; Properties
-            </h4>
-            <div className='border flex flex-col p-2'>
-                <h4 className='text-1xl font-bold'>Transform</h4>
-                <div className='flex'>
-                    <h4 className='text-1xl font-bold mr-2'>x:</h4>
-                    <Slider
-                        defaultValue={shapes[index].center.x}
-                        index={index}
-                        callback={updateAxis}
-                        callbackParams={['x']}
-                    />
-                </div>
-                <div className='flex'>
-                    <h4 className='text-1xl font-bold mr-2'>y:</h4>
-                    <Slider
-                        defaultValue={shapes[index].center.y}
-                        index={index}
-                        callback={updateAxis}
-                        callbackParams={['y']}
-                    />
-                </div>
-                <div className='flex'>
-                    <h4 className='text-1xl font-bold mr-2'>z:</h4>
-                    <Slider
-                        defaultValue={shapes[index].center.z}
-                        index={index}
-                        callback={updateAxis}
-                        callbackParams={['z']}
-                    />
-                </div>
-            </div>
-            <div className='border flex flex-col p-2'>
-                <h4 className='text-1xl font-bold'>Radius</h4>
-                <div className='flex'>
-                    <h4 className='text-1xl font-bold mr-2'>x:</h4>
-                    <Slider
-                        defaultValue={shapes[index].radius}
-                        index={index}
-                        callback={updateRadius}
-                        min={0}
-                    />
-                </div>
-            </div>
-            <div className='border flex flex-col p-2'>
-                <h4 className='text-1xl font-bold mr-2'>Colour</h4>
-                <ColorPicker
-                    color={color}
-                    onChange={setColor}
-                    hideAlpha={true}
-                    hideInput={true}
-                />
             </div>
         </div>
     );
