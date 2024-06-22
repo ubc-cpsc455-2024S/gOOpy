9ba3413ca934f1576b7826ae2085886761e736e9
Matt <73326282+MattWanJH@users.noreply.github.com>
2024-06-21 17:03:44 -0700

    halway through fixing login, logout button

--- gOOpy-frontend/src/App.jsx
+++ gOOpy-frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
+import PageHeader from './components/PageHeader';
 
 export default function App() {
     return (
         <>
-            <header className='sticky top-0 z-50 flex justify-between items-center bg-hd-color p-2'>
-                <Link to='/' className='text-3xl font-bold'>
-                    gOOpy
-                </Link>
-                <Link to='/login'>Login</Link>
-            </header>
+            <PageHeader />
             <Outlet />
         </>
     );
