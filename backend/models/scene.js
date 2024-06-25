7e79f64330fc4b4e00759c9f32d2f568a7aa0ec8
sl-81 <s_li1992@hotmail.com>
2024-06-24 22:37:23 -0700

    user schema ready

--- backend/models/scene.js
+++ backend/models/scene.js
@@ -2,24 +2,27 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const VectorSchema = new Schema({
+const vectorSchema = new Schema({
     x: { type: Number, required: true },
     y: { type: Number, required: true },
     z: { type: Number, required: true },
 });
 
-const ShapeSchema = new Schema({
-    center: { type: VectorSchema, required: true },
+const shapeSchema = new Schema({
+    center: { type: vectorSchema, required: true },
     radius: { type: Number, required: true },
     id: { type: Number, required: true },
 });
 
 // TODO: will need to update with things like camera, colour etc
-const SceneSchema = Schema({
-    shapes: [ShapeSchema],
+const sceneSchema = Schema({
+    shapes: [shapeSchema],
     metadata: {
         userID: String,
         title: String,
         lastEdited: { type: Date, default: Date.now },
+        thumbnail: String, // should this be a link to the thumbnail image?
     },
 });
+
+module.exports = mongoose.model('Scene', sceneSchema);
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    user_id: String,
-    name: String,
+const userSchema = new Schema({
+    user_id: { type: String, unique: true, required: true },
+    name: { type: String, required: true },
     description: String,
+    scenes: [{ type: Schema.Types.ObjectId, ref: 'Scene' }],
+    // TODO: might have to store oauth access tokens, refresh tokens etc
 });
+
+module.exports = mongoose.model('User', userSchema);
