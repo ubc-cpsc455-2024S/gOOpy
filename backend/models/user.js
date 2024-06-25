c7410b7047808b09c6988068ce7daff3f420c659
sl-81 <s_li1992@hotmail.com>
2024-06-24 22:43:22 -0700

    update user

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
     user_id: { type: String, unique: true, required: true },
     name: { type: String, required: true },
     description: String,
+    profile_Pic: String,
     scenes: [{ type: Schema.Types.ObjectId, ref: 'Scene' }],
     // TODO: might have to store oauth access tokens, refresh tokens etc
 });
