0a06fc73673b80808663da6c74fa6cd8b46cd9b8
sl-81 <s_li1992@hotmail.com>
2024-07-08 20:34:14 -0700

    update userSchema

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    user_id: { type: String, unique: true, required: true },
+    oauth_id: { type: String, unique: true, required: true },
     name: { type: String, required: true },
     description: String,
     profile_pic: String,
