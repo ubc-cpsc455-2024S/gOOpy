fd007869e22d9b6cbce1909ab43bcdec4a2ae9d7
Shiyu Li <96606264+sl-81@users.noreply.github.com>
2024-07-12 16:11:41 -0700

    Setup admin user and model saving (#58)
    * configure oauth2 and passport
    
    * initial scene schema ready for review
    
    * user schema ready
    
    * update user
    
    * decapitalize names
    
    * remove oauth stuff
    
    * standardize to snake case
    
    * configured .env file, confirmed db connections
    
    * fixed shapeSchema, removed dependencies
    
    * update userSchema
    
    * remove id
    
    * id and nextID
    
    * update to snake case
    
    * set up get user by id successfully
    
    * set up get specific user
    
    * minor changes
    
    * configure oauth2 and passport
    
    * initial scene schema ready for review
    
    * user schema ready
    
    * remove oauth stuff
    
    * standardize to snake case
    
    * configured .env file, confirmed db connections
    
    * fixed shapeSchema, removed dependencies
    
    * remove id
    
    * id and nextID
    
    * update to snake case
    
    * set up get user by id successfully
    
    * set up get specific user
    
    * minor changes
    
    * update after rebase
    
    * fix endpoint
    
    * successfully saved scene under goopyadmin
    
    * remove consolelogs
    
    * Update scene.js
    
    * changed users to be lowercase
    
    ---------
    
    Co-authored-by: Matt W <73326282+MattWanJH@users.noreply.github.com>

--- backend/routes/scene.js
+++ backend/routes/scene.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const sceneModel = require('../models/scene');
+const userModel = require('../models/user');
 
 // set up fake data for scenes (not linked to users, just for persistence right now)
 
@@ -22,12 +24,46 @@ router.get('/:id', (req, res) => {
     res.json(currentScene);
 });
 
-router.post('/', (req, res) => {
-    // TODO: save the scene to the current logged in user's scenes
-
-    // let scenedata = JSON.parse(req.body);
+router.post('/', async (req, res) => {
+    // TODO: save the scene to the current logged in user's scenes\
     currentScene = req.body;
-    res.send('adding a new scene');
+    shapes = [];
+
+    currentScene.shapes.map((currShape) => {
+        vec = currShape.center;
+        property = currShape.radius;
+        id = currShape.id;
+        shape = {
+            center: vec,
+            property1: property,
+            id: id,
+        };
+        shapes.push(shape);
+    });
+    const scene = {
+        shapes: shapes,
+        metadata: currentScene.metadata,
+    };
+
+    try {
+        // add to scene db
+        const savedScene = await new sceneModel(scene).save();
+        // add to user's scene
+
+        const updatedUser = await userModel.findByIdAndUpdate(
+            currentScene.metadata.user_id,
+            { $push: { scenes: savedScene._id } }, // Push the new scene's ID into the user's scenes array
+            { new: true } // Return the updated document
+        );
+
+        if (!updatedUser) {
+            res.status(404).send('User not found');
+        }
+        res.status(201).send('added scene successfully');
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('failed to add scene');
+    }
 });
 
 router.patch('/:id', (req, res) => {
