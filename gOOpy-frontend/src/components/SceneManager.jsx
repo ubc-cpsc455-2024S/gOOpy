5fcbffb937b3bea37b86bcbbb42f5a7f06716181
jlacsamana <63628806+jlacsamana@users.noreply.github.com>
2024-07-12 21:09:40 -0700

    Scene property controls (#53)
    * implement tab view controls for editor
    
    * changed border thickness to be more consistent
    
    * fix naming error
    
    * implement toggleable view component
    
    * implement skybox color control
    
    * converted slider to a standalone component
    
    * implement scene lighting control
    
    * implement ambient light intensity controls
    
    * fixed sync error with slider
    
    * cleanup
    
    * implement ui for properties/access control
    
    * addressing pr comments
    
    * Rename SceneManager.Jsx to SceneManager.jsx
    
    manaal change to lower case to fix error
    
    * reworked editor layout
    
    * changed canvas size
    
    * editor scroll fixes
    
    * remove simplecallback for slider
    
    * refactored to fit callback

---
+++ gOOpy-frontend/src/components/SceneManager.jsx
@@ -0,0 +1,83 @@
+import { ColorPicker, useColor } from 'react-color-palette';
+import GoopyButton from './GoopyButton';
+import ToggleView from './ToggleView';
+import Slider from './Slider';
+import EditorTabCarousel from './EditorTabCarousel';
+
+function SceneManager({
+    skyboxColController,
+    skyboxColor,
+    skyboxLightController,
+    skyboxLightColor,
+    skyboxAmbientController,
+    skyboxAmbient,
+    setEditorView,
+}) {
+    return (
+        <div className='bg-panel-primary border h-full pt-1 sliders flex flex-col'>
+            <div className=''>
+                <EditorTabCarousel
+                    setEditorView={setEditorView}
+                ></EditorTabCarousel>
+            </div>
+            <div className='overflow-scroll border-r border-l grow border-b scroll-container'>
+                <ToggleView label={'Properties'} classes={'border-t mt-1'}>
+                    <div className='border-b sliders'>
+                        <h4 className='text-sm mr-2'>Information</h4>
+                        <div className=' justify-between'>
+                            <h4 className='text-xs mr-2'>Scene Name:</h4>
+                            <input type='text' style={{ maxWidth: '100%' }} />
+                        </div>
+                        <div className=' justify-between'>
+                            <h4 className='text-xs mr-2'>Scene Description:</h4>
+                            <textarea
+                                type='textarea'
+                                style={{ maxWidth: '100%' }}
+                            />
+                        </div>
+                        <h4 className='text-sm mr-2'>Access Control</h4>
+                        <div className='flex justify-between'>
+                            <h4 className='text-xs mr-2'>Allow Copying</h4>
+                            <input type='checkbox' />
+                        </div>
+                    </div>
+                </ToggleView>
+                <ToggleView label={'Scene Lighting'} classes={''}>
+                    <div style={{ maxHeight: '65vh' }}>
+                        <h4 className='text-sm mr-2'>Skybox Colour</h4>
+                        <ColorPicker
+                            color={skyboxColor}
+                            onChange={(e) => {
+                                skyboxColController(e);
+                            }}
+                            hideAlpha={true}
+                            hideInput={true}
+                        />
+                        <h4 className='text-sm mr-2 pt-2'>
+                            Skybox Light Colour
+                        </h4>
+                        <ColorPicker
+                            color={skyboxLightColor}
+                            onChange={(e) => {
+                                skyboxLightController(e);
+                            }}
+                            hideAlpha={true}
+                            hideInput={true}
+                        />
+                        <h4 className='text-sm mr-2  pt-1'>
+                            Ambient Intensity
+                        </h4>
+                        <Slider
+                            min={0.0}
+                            max={1.0}
+                            defaultValue={skyboxAmbient}
+                            callback={skyboxAmbientController}
+                        ></Slider>
+                    </div>
+                </ToggleView>
+            </div>
+        </div>
+    );
+}
+
+export default SceneManager;
---
+++ gOOpy-frontend/src/components/ToggleView.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import GoopyButton from './GoopyButton';
+function ToggleView({ label, classes, children }) {
+    const [showing, toggleView] = useState(false);
+    return (
+        <div className={`${classes}`}>
+            <GoopyButton
+                onClick={() => {
+                    toggleView(!showing);
+                }}
+                classes={`border-b button cursor-pointer flex w-full`}
+            >
+                <h4 className='text-xl'>{label}</h4>
+            </GoopyButton>
+            {showing && children}
+        </div>
+    );
+}
+
+export default ToggleView;
