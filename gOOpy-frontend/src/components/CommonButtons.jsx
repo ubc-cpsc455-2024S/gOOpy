76cb1b33d79005d9cb9892318fe2a807b1b14a7b
AidenKerr <arkerr2000@gmail.com>
2024-08-05 19:14:25 -0700

    refactor SVGs

--- gOOpy-frontend/src/components/CommonButtons.jsx
+++ gOOpy-frontend/src/components/CommonButtons.jsx
@@ -6,23 +6,6 @@ import { SHAPE_TYPES } from '../pages/Editor/constants';
 const MAX_SHAPES = 50; // should match shaders
 const DOWNLOAD_FILE_TYPE = 'png';
 
-function SceneSaveSVG() {
-    return (
-        <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='green'
-            className='size-6'
-        >
-            <path
-                fillRule='evenodd'
-                d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z'
-                clipRule='evenodd'
-            />
-        </svg>
-    );
-}
-
 function handleDownload() {
     const canvasHeight = document.getElementsByTagName('canvas')[0].height;
     const data = createImageDataURL(canvasHeight, 'png');
@@ -108,7 +91,7 @@ export const CommonButtons = ({
             >
                 <div className='flex justify-between'>
                     <h1>Save Scene</h1>
-                    {sceneSaved && <SaveSceneSVG />}
+                    {sceneSaved && <SaveSVG />}
                 </div>
             </GoopyButton>
             <GoopyButton
@@ -120,3 +103,21 @@ export const CommonButtons = ({
         </div>
     );
 };
+
+// SVG from heroicons
+function SaveSVG() {
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='green'
+            className='size-6'
+        >
+            <path
+                fillRule='evenodd'
+                d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z'
+                clipRule='evenodd'
+            />
+        </svg>
+    );
+}
